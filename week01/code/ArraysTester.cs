6f3daf67abab505f7575f5cd7429c9dee48351ef
wedding-site <sayraprogramacion@gmail.com>
2024-03-01 17:44:17 -0600

    code week 01 part2

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -73,7 +73,11 @@ public static class ArraysTester {
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-            for (int i=0; i<data.Count; ++i){
+          
+        // 1-create a loop for the data list, declaring the variable int "j", and using as condition the lenght of data list".
+        // 2-Find the index of amount
+        // 3-Use the Reverse method, using the index of amount and the size of the list
+            for (int j=0; j<data.Count; ++j){
             data.Reverse(amount, 8);
             
             }
